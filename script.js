e6b8b8ad105ad8a53cbe3bfa8ea5b436636a0cfd
shaik thaheer <147476099+thaheer786@users.noreply.github.com>
2025-10-13 23:47:47 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
   // Smooth scrolling for navigation links  
         document.querySelectorAll('a[href^="#"]').forEach(anchor => {
             anchor.addEventListener('click', function (e) {
-                e.preventDefault();
+                e.preventDefault(); 
                 const target = document.querySelector(this.getAttribute('href'));
                 if (target) {
                     target.scrollIntoView({
